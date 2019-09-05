fdf8798480df69fffae258607a0125409292fd0f
Kristin <kberry@usgs.gov>
2019-09-05 09:42:22 -0700

    Updated cassini test again

--- tests/pytests/test_cassini_drivers.py
+++ tests/pytests/test_cassini_drivers.py
@@ -1,6 +1,5 @@
 import pytest
 import ale
-
 import os
 
 import numpy as np
@@ -16,18 +15,40 @@ from conftest import get_image_kernels, convert_kernels, get_image_label
 @pytest.fixture()
 def usgscsm_compare_dict():
     return {'radii': 
-            {'semimajor': 256.6, 
-             'semiminor': 248.3, 
-             'unit': 'km'}, 
+             {'semimajor': 256.6, 
+               'semiminor': 248.3, 
+               'unit': 'km'}, 
             'sensor_position': 
-            {'positions': [np.array([ -2256954.95303959, -24306346.83742413,   -639245.80662036])], 
-             'velocities': [np.array([-6890.54418209, -2939.21925245,    21.83699238])], 
-             'unit': 'm'}, 
+              {'positions': [np.array([ -2256954.95303959, -24306346.83742413,   -639245.80662036])], 
+               'velocities': [np.array([-6890.54418209, -2939.21925245,    21.83699238])], 
+               'unit': 'm'}, 
             'sun_position': 
-            {'positions': [np.array([-2.30714008e+11, -1.39662228e+12,  3.09872114e+11])], 
-             'velocities': [np.array([-7.41139016e+07,  1.22563268e+07,  3.85603215e+03])], 'unit': 'm'}, 
+              {'positions': [np.array([-2.30714008e+11, -1.39662228e+12,  3.09872114e+11])], 
+               'velocities': [np.array([-7.41139016e+07,  1.22563268e+07,  3.85603215e+03])], 
+               'unit': 'm'}, 
             'sensor_orientation': 
-            {'quaternions': [[-0.21183452, -0.66567547, -0.69966839,  0.14988809]]}, 'detector_sample_summing': 1, 'detector_line_summing': 1, 'focal_length_model': {'focal_length': 2003.09}, 'detector_center': {'line': 511.5, 'sample': 511.5}, 'starting_detector_line': 0, 'starting_detector_sample': 0, 'focal2pixel_lines': [0.0, 0.0, 83.33333333333333], 'focal2pixel_samples': [0.0, 83.33333333333333, 0.0], 'optical_distortion': {'radial': {'coefficients': [-8e-06, 0, 0]}}, 'image_lines': 1024, 'image_samples': 1024, 'name_platform': 'CASSINI ORBITER', 'name_sensor': 'IMAGING SCIENCE SUBSYSTEM - NARROW ANGLE', 'reference_height': {'maxheight': 1000, 'minheight': -1000, 'unit': 'm'}, 'name_model': 'USGS_ASTRO_FRAME_SENSOR_MODEL', 'center_ephemeris_time': 376938208.24072826}
+              {'quaternions': [[-0.21183452, -0.66567547, -0.69966839,  0.14988809]]}, 
+            'detector_sample_summing': 1, 
+            'detector_line_summing': 1, 
+            'focal_length_model': {'focal_length': 2003.09}, 
+            'detector_center': {'line': 511.5, 'sample': 511.5}, 
+            'starting_detector_line': 0, 
+            'starting_detector_sample': 0, 
+            'focal2pixel_lines': [0.0, 0.0, 83.33333333333333], 
+            'focal2pixel_samples': [0.0, 83.33333333333333, 0.0], 
+            'optical_distortion': 
+              {'radial': 
+                {'coefficients': [-8e-06, 0, 0]}}, 
+            'image_lines': 1024, 
+            'image_samples': 1024, 
+            'name_platform': 'CASSINI ORBITER', 
+            'name_sensor': 'IMAGING SCIENCE SUBSYSTEM - NARROW ANGLE', 
+            'reference_height': 
+              {'maxheight': 1000, 
+               'minheight': -1000, 
+               'unit': 'm'}, 
+            'name_model': 'USGS_ASTRO_FRAME_SENSOR_MODEL', 
+            'center_ephemeris_time': 376938208.24072826}
 
 @pytest.fixture()
 def test_kernels(scope="module", autouse=True):
@@ -56,15 +77,15 @@ class test_cassini_pds3_naif(unittest.TestCase):
         assert self.driver.spacecraft_name == "CASSINI"
     
     def test_focal2pixel_samples(self):
-        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
+        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[12.0]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-             assert self.driver.focal2pixel_samples == [0.0, 100.0, 0.0]
+             assert self.driver.focal2pixel_samples == [0.0, 83.33333333333333, 0.0]
              gdpool.assert_called_with('INS-12345_PIXEL_SIZE', 0, 1)
              
     def test_focal2pixel_lines(self):
-        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
+        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[12.0]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-             assert self.driver.focal2pixel_lines == [0.0, 0.0, 100.0]
+             assert self.driver.focal2pixel_lines == [0.0, 0.0, 83.33333333333333]
              gdpool.assert_called_with('INS-12345_PIXEL_SIZE', 0, 1)
     
     def test_odtk(self):
@@ -74,33 +95,29 @@ class test_cassini_pds3_naif(unittest.TestCase):
         assert self.driver.instrument_id == "CASSINI_ISS_NAC"
     
     def test_focal_epsilon(self):
-        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
+        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[0.03]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-             assert self.driver.focal_epsilon == 10
+             assert self.driver.focal_epsilon == 0.03
              gdpool.assert_called_with('INS-12345_FL_UNCERTAINTY', 0, 1)
     
     def test_focal_length(self):
+        # This value isn't used for anything in the test, as it's only used for the
+        # default focal length calculation if the filter can't be found.
         with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
              assert self.driver.focal_length == 2003.09
     
     def test_detector_center_sample(self):
-        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
+        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[511.5, 511.5]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-             assert self.driver.detector_center_sample == 10
+             assert self.driver.detector_center_sample == 511.5
              gdpool.assert_called_with('INS-12345_FOV_CENTER_PIXEL', 0, 2)
     
     def test_detector_center_line(self):
-        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
+        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[511.5, 511.5]) as gdpool, \
              patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-             assert self.driver.detector_center_sample == 10
+             assert self.driver.detector_center_sample == 511.5
              gdpool.assert_called_with('INS-12345_FOV_CENTER_PIXEL', 0, 2)
-    
-#    def test_frame_chain(self):
-#        with patch('ale.drivers.co_drivers.spice.gdpool', return_value=[10.0]) as gdpool, \
-#             patch('ale.base.data_naif.spice.bods2c', return_value=-12345) as bods2c:
-#             assert self.driver.frame_chain == 10
-#             gdpool.assert_called_with('INS-12345_FOV_CENTER_PIXEL', 0, 2)
     
     def test_sensor_model_version(self):
         assert self.driver.sensor_model_version == 1
